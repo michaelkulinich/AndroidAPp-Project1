7adfe8a88fca90f6b5f13dca2b65486f5acaafaf
Michael Kulinich <kulinich@chapman.edu>
2019-09-17 18:47:11 -0700

    First Check in

--- app/src/main/java/com/example/firstprogram/MainActivity.kt
+++ app/src/main/java/com/example/firstprogram/MainActivity.kt
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.view.isVisible
+import android.util.Log
+
+
 
 class MainActivity : AppCompatActivity() {
-    var count = 0;
+    private var count: Long = 0;
+    //fun getStore() = getPreferences(Context.MODE_PRIVATE)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState !=null){
+            count = savedInstanceState.getLong(myCounter_key, 0)
+            myCounter.text = count.toString();
+        }
 
         myButton.setOnClickListener {
 
@@ -21,4 +31,21 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+//    override fun onPause() {
+//        super.OnPause()
+//        store.edit().putLong(myCounter_key, count).apply()
+//    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.run {
+            putLong(myCounter_key, count)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    companion object{
+        private const val myCounter_key = "myCounterKey"
+    }
+
+
 }

03ac0f2c2b110545b0830659dbbd0bd24f0e25bf
Michael Kulinich <kulinich@chapman.edu>
2019-11-11 15:55:08 -0800

    check in 7-8

--- app/src/main/java/com/example/firstprogram/Ui/MainActivity.kt
+++ app/src/main/java/com/example/firstprogram/Ui/MainActivity.kt
@@ -8,7 +8,18 @@ import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
 import com.example.firstprogram.R
+import com.giphy.sdk.core.models.Media
+import com.giphy.sdk.core.models.enums.RenditionType
+import com.giphy.sdk.ui.GPHContentType
+import com.giphy.sdk.ui.GPHContentType.*
+import com.giphy.sdk.ui.GPHSettings
+import com.giphy.sdk.ui.GiphyCoreUI
+import com.giphy.sdk.ui.themes.GridType
+import com.giphy.sdk.ui.themes.GridType.*
+import com.giphy.sdk.ui.themes.LightTheme
+import com.giphy.sdk.ui.views.GiphyDialogFragment
 import java.util.*
+
 
 
 
@@ -24,6 +35,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        GiphyCoreUI.configure(this, "7Hoz4hOd8Ve9iQVOAX79kF3IsEUeombI")
+        var settings = GPHSettings(gridType = waterfall, theme = LightTheme, dimBackground = true)
+        val gifsDialog = GiphyDialogFragment.newInstance(settings)
+        settings.mediaTypeConfig = arrayOf(GPHContentType.gif)
+        gifsDialog.show(supportFragmentManager, "gifs_dialog")
+
+
+        gifsDialog.gifSelectionListener = object: GiphyDialogFragment.GifSelectionListener {
+            override fun onGifSelected(media: Media) {
+                gifImage.setMedia(media, RenditionType.original)
+            }
+
+            override fun onDismissed() {
+                //Your user dismissed the dialog without selecting a GIF
+            }
+        }
+
         setContentView(R.layout.activity_main)
         level.visibility = View.GONE
         hogImage.visibility = View.GONE
@@ -31,6 +61,9 @@ class MainActivity : AppCompatActivity() {
         raptorImage.visibility = View.GONE
         dragonImage.visibility = View.GONE
         listPrizes.visibility = View.GONE
+
+
+
         countViewModel = ViewModelProviders.of(this).get(CountViewModel::class.java)
         countViewModel.getUserCount(getUsername()).observe(this,
             androidx.lifecycle.Observer { updateCounter(it) })
